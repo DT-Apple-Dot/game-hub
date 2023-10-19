ae7e9163b1a1424f3dd3aabfa70a3ee701ad5dec
DT-Apple-Dot <NarNarismine21>
2023-10-20 00:16:18 +0630

    Highlight the selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
       templateColumns={{
-        base: "1fr",
-        lg: "200px 1fr",
+        base: `"1fr"`,
+        lg: `"200px 1fr"`,
       }}
     >
       <GridItem area="nav">
@@ -24,7 +24,10 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList onSelectedGenre={(genre) => setSelectedGenre(genre)} />
+          <GenreList
+            selectedGenre={selectedGenre}
+            onSelectedGenre={(genre) => setSelectedGenre(genre)}
+          />
         </GridItem>
       </Show>
       <GridItem area="main">
