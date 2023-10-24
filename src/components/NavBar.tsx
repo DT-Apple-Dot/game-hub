a58d700b67358b08b1c9e6fda16e94d1969b3d38
DT-Apple-Dot <NarNarismine21>
2023-10-25 01:55:01 +0630

    Implement searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface Props {
+  onSeacrch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSeacrch }: Props) => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <SearchInput />
+      <SearchInput onSeacrch={onSeacrch} />
       <ColorModeSwitch />
     </HStack>
   );
